61f7b2b968c54f62a5907af0daff3b3737629e26
Chou <65751667+FaZeDrug@users.noreply.github.com>
2024-02-16 22:58:11 -0800

    Delete testing.js

--- testing.js
+++
@@ -1,5 +0,0 @@
-function calculateDaysBetweenDates(begin, end) {
-    const millisecondsPerDay = 1000 * 60 * 60 * 24;
-    const millisecondsBetween = end - begin;
-    return Math.floor(millisecondsBetween / millisecondsPerDay);
-    }
